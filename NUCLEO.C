058d778a47f41d934c71a4787baf645682d080f1
Pedro Sorge <pedrosorge2005@gmail.com>
2026-09-01 22:55:20 -0300

    Implementating semaphores

--- NUCLEO.C
+++ NUCLEO.C
@@ -32,16 +32,60 @@ void add_to_process_list(PROCESS_LIST *pl, BCP *bcp){
 
 /* Função para achar o próximo processo disponível */
 int next_bcp(PROCESS_LIST *list){
+    int i;
     if(list->prim==NULL) return 0;
-    int aux=0;
-    while(list->prim->STATUS == blocked){
-        list->prim = list->prim->next;
-        if(++aux == list->num_process){
-            exit(0);
-            volta_dos(); /* FALTA IMPLEMENTAR*/
+    
+    list->prim = list->prim->next;
+
+    for (i = 0; i < list->num_process; i++) {
+        if (list->prim->status != blocked) {
+            return 1; /* Encontrou um processo pronto */
         }
+        list->prim = list->prim->next;
     }
-    return 1;
+
+    volta_dos();
+    exit(0);
+    return 0;
+}
+
+/* Função para inicializar fila de processos bloqueados */
+void initilize_blocked_process_queue(BLOCKED_PROCESS_QUEUE *bpq){
+    bpq->head = NULL;
+    bpq->tail = NULL;
+}
+
+/* Função para adicionar processo à fila de bloqueados */
+void add_blocked_process_to_queue(BLOCKED_PROCESS_QUEUE *bpq, PTR_DESC_PROC bcp){
+
+    BLOCKED_QUEUE_NODE *aux = (BLOCKED_QUEUE_NODE *) malloc(sizeof(BLOCKED_QUEUE_NODE)); /* Instancia nó que aponta para o processo bloqueado */
+    aux->bcp = bcp; /* Inicializa o nó */
+    aux->next = NULL;
+    bcp->status = blocked;
+
+    if(bpq->head == NULL){
+        bpq->head = aux;
+        bpq->tail = aux;
+    }
+    else{
+        bpq->tail->next = aux;
+        bpq->tail = aux;
+    }
+
+}
+
+/* Função para remover processo da fila de bloqueados */
+void remove_bloqued_process_from_queue(BLOCKED_PROCESS_QUEUE *bpq){
+
+    BLOCKED_QUEUE_NODE *temp;    
+    if(bpq->head != NULL){
+        temp = bpq->head;
+        temp->bcp->status = ready;
+        bpq->head = bpq->head->next;   
+        if(bpq->head == NULL) bpq->tail = NULL;
+        free(temp);
+    }
+
 }
 
 
@@ -49,21 +93,21 @@ int next_bcp(PROCESS_LIST *list){
 void far create_process(char name[150], void far (*end_proc)()){
     BCP *aux = (BCP *) malloc(sizeof(BCP));                     /* Aloca BCP */
     strcpy(aux->name, name);                                    /* Compia o nome do processo */
-    aux->context = (PTR_DESC_PROC) malloc(sizeof(PTR_DESC));    /* Aloca descritor do processo*/
-    *(aux->context) = cria_desc();                              /* Cria descritor do processo */
-    newprocess(end_proc, *(aux->context));                      /* Inicializa o descritor */
+    aux->context = cria_desc();                                 /* Cria descritor do processo*/
+    newprocess(end_proc, aux->context);                         /* Inicializa o descritor */
     aux->status = ready;                                        /* Inicializa o estado do processo */
     
     add_to_process_list(&process_list, aux);                    /* Adiciona o bcp na lista de processos do escalonador */
 }
 
+/* Função da corrotina do escalonador de processos */
 void far escalonator(){
     p_est->num_vetor = 8;
-    p_est->origem = dEsc;
+    p_est->p_origem = dEsc;
     
     while(1){
         if(process_list.prim != NULL){
-            p_est->p_destino = *(process_list.prim->context);
+            p_est->p_destino = process_list.prim->context;
             iotransfer(); 
         }
         if (!next_bcp(&process_list)) exit(0);
@@ -72,6 +116,7 @@ void far escalonator(){
 }
 
 
+/* Código principal */
 int main(){
     initilize_process_list(&process_list); /* Inicializa a lista de processos */
 
